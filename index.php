072a3b0d845dfc422d74b25ae837a89bd7bd8ec2
Alzheimer10 <carlosanselmi2@gmail.com>
2016-12-17 11:45:14 -0430

    Lecccion Encasulamiento, getters y settters

--- index.php
+++ index.php
@@ -5,20 +5,53 @@
 	 */
 	class Person
 	{
-		var $firstName = 'NO_DEFINE';
-		var $lastName = 'NO_DEFINE';
+		// public, protected <=> private
+		protected $firstName = 'NO_DEFINE';
+		protected $lastName = 'NO_DEFINE';
+		protected $nickname; 
 
-		public function __construct($firstName,$lastName)
+		public function __construct($firstName,$lastName,$nickname)
 		{
 			$this->firstName = $firstName;
 			$this->lastName = $lastName;
+			$this->nickname = $nickname;
 		}
 
-		public function fullName(){
-			return $this->firstName.' '. $this->lastName;
+		public function getFullName(){
+			return $this->firstName.' '. $this->lastName.' '.$this->nickname;
 		}
+
+		public function setFirstName($firstName){
+			$this->firstName = $firstName;
+		}
+
+		public function setLastName($lastName){
+			$this->lastName = $lastName;
+		}
+
+		public function setNickname($nickname){
+			$this->nickname = $nickname;
+		}
+
+		public function getFirstName(){
+			return $this->firstName;
+		}
+
+		public function getLastName(){
+			return $this->lastName;
+		}
+
+		public function getNickname(){
+			return strtolower($this->nickname);
+		}
+
+
 	}
 
-	$person1 = new Person('Carlos','Anselmi');
-	echo $person1->fullName();
-	echo "<br>{$person1->firstName} tiene como apellido {$person1->lastName}";
+	$person1 = new Person('Carlos','Anselmi','Alzheimer');
+	echo $person1->getFullName();
+	$person1->setFirstName('Alfredo');
+	$person1->setLastName('Jimenez');
+	$person1->setNickname('Alzheimer10');
+	echo "<br>".$person1->getNickname();
+	echo "<br>{$person1->getFirstName()} tiene como apellido {$person1->getLastName()}";
